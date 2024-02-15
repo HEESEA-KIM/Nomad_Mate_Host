4f672df9b9bc52550129cec3338a61229408ef94
HyunJunLee <guswns0414@naver.com>
2024-02-15 21:55:37 +0900

    functions 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nomad/app.dart';
 import 'firebase_options.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'assets/config/.env');
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // FCM 초기화
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // 사용자에게 알림 권한 요청
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  // FCM 등록 토큰 얻기
+  String? token = await messaging.getToken();
+  print("FCM Token: $token"); // 콘솔에 토큰 출력
+
   runApp(const HostApp());
 }
 
